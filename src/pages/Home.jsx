d8f75e3509b06a59017a3c071cbf27436dd9f134
Ruman-Islam <rumanislam48@gmail.com>
2022-07-23 05:32:03 +0600

    font awesome installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComputer } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
     return (
         <div>
             <h1>This is home page</h1>
+            <FontAwesomeIcon icon={faComputer} />
         </div>
     );
 };
